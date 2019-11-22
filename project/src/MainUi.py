37bd1c7a89e87c5cf3e382f4e2ef9485d931061a
Tim <tim-3.schneider@uni-ulm.de>
2019-11-22 20:40:50 +0100

    Added PycharmProject with simple PDF-view capabilities

---
+++ project/src/MainUi.py
@@ -0,0 +1,87 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeyEvent, QIntValidator
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QHBoxLayout, QWidget, QToolBar, QAction, qApp, \
+    QFileDialog, QLineEdit, QSizePolicy
+
+
+from src.PdfDrawWidget import PdfDrawWidget
+
+
+class MainUI(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        verticalLayout = QVBoxLayout()
+        horizontalLayout = QHBoxLayout()
+
+        # Todo Add Draw function for MousePointers of others
+        # The central Widget where all content is displayed
+        centralwidget = QWidget(self)
+        centralwidget.setObjectName("centralwidget")
+        centralwidget.setLayout(verticalLayout)
+
+        #Create the pdfWiget which Displays the PDF
+        self.pdfWidget = PdfDrawWidget(centralwidget)
+
+        #Set Layout Design
+        horizontalLayout.addWidget(self.pdfWidget.verticalScrollbar)
+        horizontalLayout.addWidget(self.pdfWidget)
+
+        verticalLayout.addLayout(horizontalLayout)
+        verticalLayout.addWidget(self.pdfWidget.horizontalScrollbar)
+
+        self.toolbar: QToolBar = self.addToolBar("Toolbar1")
+        self.fillToolbar()
+
+
+        self.setCentralWidget(centralwidget)
+        self.resize(800,600)
+        self.setWindowTitle('PdfViewer')
+        self.show()
+
+
+    intvalidator: QIntValidator = QIntValidator()
+    pageNum: QLineEdit = None
+
+    def fillToolbar(self):
+        exitAct = QAction('Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.triggered.connect(qApp.quit)
+
+        openAct = QAction('Open', self)
+        openAct.setShortcut('Ctrl+O')
+        openAct.triggered.connect(self.getPath)
+
+
+        self.intvalidator.setRange(0,0)
+        self.pageNum = QLineEdit(self.toolbar)
+        self.pageNum.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.pageNum.setValidator(self.intvalidator)
+        self.pageNum.setFocusPolicy(Qt.ClickFocus)
+
+        self.toolbar.addAction(exitAct)
+        self.toolbar.addAction(openAct)
+        self.toolbar.addWidget(self.pageNum)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        """Connect all KeyPressEvents to the self.pdfWidget"""
+        self.pdfWidget.keyPressEvent(event)
+
+    def keyReleaseEvent(self, event: QKeyEvent):
+        """Connect all KeyReleaseEvents to the self.pdfWidget"""
+        self.pdfWidget.keyReleaseEvent(event)
+
+
+    def getPath(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        if fname[0]:
+            self.pdfWidget.loadDocument(fname[0])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainUI()
+    sys.exit(app.exec_())
---
+++ project/src/PdfDrawWidget.py
@@ -0,0 +1,141 @@
+from typing import List, Tuple
+
+from PyQt5.QtCore import QRect, Qt, QPoint
+from PyQt5.QtGui import QPainter, QImage, QPaintEvent, QWheelEvent, QKeyEvent, QMouseEvent
+from PyQt5.QtWidgets import QWidget, QScrollBar
+from fitz import Document, Page, Pixmap, fitz, Matrix, Rect
+
+
+class PdfDrawWidget(QWidget):
+    horizontalScrollbar = None
+    verticalScrollbar = None
+
+    pdfVis: Document = None
+    pageNum: int = 0
+    zoom: float = 1
+
+    __controlIsPressed = False
+
+    __pressStart: Tuple[float, float] = None
+
+    def __init__(self, parent: QWidget = None, painter: QPainter = QPainter()):
+        super().__init__(parent)
+        self.painter = painter
+        self.horizontalScrollbar = QScrollBar(Qt.Horizontal)
+        self.verticalScrollbar = QScrollBar(Qt.Vertical)
+        self.horizontalScrollbar.setVisible(False)
+        self.verticalScrollbar.setVisible(False)
+
+        self.horizontalScrollbar.valueChanged.connect(lambda: self.update())
+        self.verticalScrollbar.valueChanged.connect(lambda: self.update())
+        self.setMouseTracking(True)
+
+    def paintEvent(self, event: QPaintEvent):
+        """All Drawing Actions are activated here"""
+        self.__drawPDF(event)
+
+    __viewchanged = True
+    __zoomed = True
+    __pdfImages: List[Tuple[int, QImage]] = []  # List of Tuples where first index is the hightoffset
+    __pageoffsets: List[int] = []
+
+    def __drawPDF(self, event: QPaintEvent):
+        """Draws the Pdf centered onto the self Object as long as the self.pdfVis variable isn't None.
+           self.verticalScrollbar &  self.horizontalScrollbar will be manipulated in range and visibility"""
+        if self.pdfVis is not None:
+
+            if self.__viewchanged:
+                if self.__zoomed:
+                    hightCount = 0
+                    self.__pageoffsets = [0]
+                    for page in self.pdfVis.pages():
+                        hightCount += page.rect.height * self.zoom
+                        self.__pageoffsets.append(hightCount)
+                    hightCount -= self.height()
+                    if self.height() < hightCount:
+                        self.verticalScrollbar.setRange(0, hightCount)
+                        self.verticalScrollbar.setVisible(True)
+                    else:
+                        self.verticalScrollbar.setVisible(False)
+                        self.verticalScrollbar.setRange(0, 0)
+
+            mat = fitz.Matrix(self.zoom, self.zoom)
+
+            self.__pdfImages = []
+            lowestVisablePage = next(i for i in range(len(self.__pageoffsets) - 1) if
+                                     self.__pageoffsets[i + 1] >= self.verticalScrollbar.value())
+            try:
+                highestVisablePage = next(i for i in range(len(self.__pageoffsets) - 1) if self.__pageoffsets[
+                    i + 1] >= self.verticalScrollbar.value() + self.height()) + 1
+            except StopIteration:
+                highestVisablePage = self.pdfVis.pageCount
+
+            for falseI, page in enumerate(self.pdfVis.pages(lowestVisablePage, highestVisablePage)):
+                i = falseI + lowestVisablePage
+                clipx0 = (page.rect.width * self.zoom / 2 - self.width() / 2 + self.horizontalScrollbar.value()) \
+                    if (
+                                   page.rect.width * self.zoom / 2 - self.width() / 2 + self.horizontalScrollbar.value()) > 0 else 0
+                clipy0 = (self.verticalScrollbar.value() - self.__pageoffsets[i]) / self.zoom \
+                    if self.verticalScrollbar.value() >= self.__pageoffsets[i] else 0
+                # clipx1 =
+                # clipy1 =
+                print(clipy0, clipx0)
+                # print(self.verticalScrollbar.value())
+                pix: Pixmap = page.getPixmap(mat)  # , clip=Rect(0,0,110,110))
+                fmt = QImage.Format_RGBA8888 if pix.alpha else QImage.Format_RGB888
+                self.__pdfImages.append(
+                    (self.__pageoffsets[i], QImage(pix.samples, pix.width, pix.height, pix.stride, fmt)))
+
+            if self.__pdfImages[0][1].width() > self.width():
+                self.horizontalScrollbar.setRange(self.width() - self.__pdfImages[0][1].width(),
+                                                  self.__pdfImages[0][1].width() - self.width())
+                self.horizontalScrollbar.setVisible(True)
+            else:
+                self.horizontalScrollbar.setVisible(False)
+                self.horizontalScrollbar.setRange(0, 0)
+
+                self.__viewchanged = False
+
+            self.painter.begin(self)
+            for i, (y, img) in enumerate(self.__pdfImages):
+                self.painter.drawImage(self.painter.viewport().width() / 2 - img.width() / 2
+                                       - self.horizontalScrollbar.value(),
+                                       y + 5 * i - self.verticalScrollbar.value(), img)
+            self.painter.end()
+        else:
+            pass
+
+    def resizeEvent(self, QResizeEvent):
+        super().resizeEvent(QResizeEvent)
+
+    def wheelEvent(self, event: QWheelEvent):
+        if self.__controlIsPressed:
+            if self.zoom + 0.005 * event.angleDelta().y() > 0:
+                self.zoom += 0.005 * event.angleDelta().y()
+            else:
+                self.zoom = 0.1
+            self.__zoomed = True
+        else:
+            self.verticalScrollbar.setValue(self.verticalScrollbar.value() - event.angleDelta().y())
+        self.__viewchanged = True
+        self.update()
+
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key_Control:
+            self.__controlIsPressed = True
+
+    def keyReleaseEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key_Control:
+            self.__controlIsPressed = False
+
+    def mousePressEvent(self, event: QMouseEvent):
+        self.__pressStart = (event.x(), event.y())
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        if self.__pressStart is not None:
+            pass
+            # Todo Draw Rectangle around all Textboxes
+
+    def loadDocument(self, path: str):
+        self.pdfVis = fitz.Document(path)
+        self.update()
