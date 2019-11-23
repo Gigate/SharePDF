58c1103e519e556d72d1b48b0b4228d897c0cd70
Tim <tim-3.schneider@uni-ulm.de>
2019-11-23 19:56:46 +0100

    chnged MessageTypes

--- src/MessageTyp.py
+++ src/MessageTyp.py
@@ -1,28 +1,31 @@
+from typing import Tuple
+
 from fitz import Document
 
 
-class Lobby:
-    abbr = "lby"
+class LobbyConnect:
     pdf: Document = None
+    user_id = None
 
-    def __init__(self, lobbyname: str, password: str):
+    def __init__(self, lobby_name: str, password: str, username: str, pdf: Document = None):
         self.password: str = password
-        self.lobbyname: str = lobbyname
+        self.lobby_name: str = lobby_name
+        self.username = username
+        self.pdf = pdf
 
 
 class ClientStatus:
-    abbr = "cst"
-    mouse_pos: (float, float)
+    mouse_pos: Tuple[float, float]
     marker_objects = []
     test = None
 
-    def __init__(self, mouse_pos, marker_objects=[]):
+    def __init__(self, mouse_pos: Tuple[float, float], user_id: int, marker_objects=[]):
         self.marker_objects = marker_objects
         self.mouse_pos = mouse_pos
+        self.user_id = user_id
 
 
 class ServerStatus:
-    abbr = "cst"
     code: int
     message: str
     changed_users: dict = {}
